1dd914eaac22f0d38a8470da50a58d2e173b3297
abebaw1616@gmail.com <Local Admin@ETNTSRSLP061361.ethio.local>
2024-09-29 22:22:55 +0300

    Header and Footer

--- netflix-clon/src/App.js
+++ netflix-clon/src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import  Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-<h1> hello abebaw </h1>      
+     <Home/>
     </div>
   );
 }
---
+++ netflix-clon/src/pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../../components/Header/Header'
+import  Footer  from '../../components/Footer/Footer'
+
+export const Home = () => {
+  return (
+    <>
+
+<Header/>
+<Footer/>
+    </>
+  )
+}
+
+export default Home
